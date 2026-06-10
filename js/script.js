d72617540b905b5c1c29ffbc4f6a15d0ceb9e5c0
aming <1474748385@qq.com>
2026-06-10 23:30:11 +0800

    1

--- js/script.js
+++ js/script.js
@@ -92,12 +92,6 @@
 			$(card_item).addClass('active');
 		}
 
-		if(!menu_item_home.hasClass('active')) {
-			$(innerbox).addClass('offsetleft');
-		}else {
-			$(innerbox).removeClass('offsetleft');
-		}
-		
 		return false;
 	});
 
